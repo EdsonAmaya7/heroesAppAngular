26ffd7d1910693d516c1651f7796782fcb8f0556
EdsonAmaya7 <edson_amaya99@live.com>
2023-02-10 16:06:34 -0600

    falta el frontend

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IAuth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +17,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get auth(){
+    return this.authService.auth;
   }
 
+  constructor(private router: Router,
+              private authService:AuthService
+    ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  logout() {
+    this.router.navigate(['./auth'])
+  }
 }
